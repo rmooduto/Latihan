384603824f98bf5aba9a15abdcf9715f4a77a5b1
rizki mooduto <rizki.mhs16@ti.poligon.ac.id>
2019-02-01 02:10:43 -0500

    edit data1

---
+++ src/main/java/com/rmooduto/apliksipmb/ApliksiPmbApplication.java
@@ -0,0 +1,14 @@
+package com.rmooduto.apliksipmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApliksiPmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApliksiPmbApplication.class, args);
+	}
+
+}
+
